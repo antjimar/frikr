27055b54aba4b2ba087e5b742dfba2a5b26ff189
Alberto Casero <alberto@kasfactory.net>
2014-05-31 16:29:43 +0200

    Estáticos y admin tunning

--- frikr/photos/admin.py
+++ frikr/photos/admin.py
@@ -1,4 +1,36 @@
+# -*- coding: utf-8 -*-
 from django.contrib import admin
 from models import Photo
 
-admin.site.register(Photo) # Add Photo to admin web interface
+
+class PhotoAdmin(admin.ModelAdmin):
+
+    # list customization
+    list_display = ('name', 'owner_name', 'license', 'visibility')
+    list_filter = ('license', 'visibility')
+    search_fields = ('name', 'description')
+
+    def owner_name(self, obj):
+        return obj.owner.first_name + ' ' + obj.owner.last_name
+
+    owner_name.short_description = 'Propietario'
+    owner_name.admin_order_field = 'owner'
+
+
+    # detail customization
+    fieldsets = (
+        (None, {
+            'fields' : ('name', 'url', 'owner'),
+            'classes' : ('wide',)
+        }),
+        ('Descripción', {
+            'fields' : ('description',),
+            'classes': ('wide',)
+        }),
+        ('Licencia y visibilidad', {
+            'fields' : ('license', 'visibility'),
+            'classes': ('wide', 'collapse')
+        })
+    )
+
+admin.site.register(Photo, PhotoAdmin) # Add Photo to admin web interface
